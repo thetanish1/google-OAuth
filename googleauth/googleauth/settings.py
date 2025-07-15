b42fb42506466dc4942ed1d725444a85c9c94223
thetanish1 <tanishdewase25@gmail.com>
2025-07-16 00:39:14 +0530

    migration and migrate the code

--- googleauth/googleauth/settings.py
+++ googleauth/googleauth/settings.py
@@ -67,6 +67,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+
 ]
 
 ROOT_URLCONF = 'googleauth.urls'
